6ee40e31bd13d8880884d9b0bd6fe8c3d676ff6d
rubsmaster <jrubsb@gmail.com>
2025-01-27 17:36:55 -0600

    imagenes del proyecto

--- obj/Debug/net8.0/mayanBoats.AssemblyInfo.cs
+++ obj/Debug/net8.0/mayanBoats.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mayanBoats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95443d127cddb28e38304b1368071cd7dd5c63bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("mayanBoats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mayanBoats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
