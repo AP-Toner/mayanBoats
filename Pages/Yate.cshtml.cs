95443d127cddb28e38304b1368071cd7dd5c63bd
rubsmaster <jrubsb@gmail.com>
2025-01-27 17:21:41 -0600

    base project

---
+++ Pages/Yate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mayanBoats.Pages
+{
+    public class YateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
